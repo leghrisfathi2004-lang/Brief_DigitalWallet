3bab46a1b97bf63f48fadc685503d66f9a648b3b
fathi leghris <leghrisfathi2004@gmail.com>
2026-02-21 10:08:54 +0000

    server done

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const http = require('http');
-const userRt = require('./routes/users.routes.js');
-const walletRt = require('./routes/wallets.routes.js');
+import http from 'http';
+import { userRt } from'./routes/users.routes.js';
+import { walletRt } from './routes/wallets.routes.js';
 
 
 let server = http.createServer((req, res) => {
+    if(req.url.startsWith('/wallets') || req.url.includes('/wallets')) {
+        return walletRt(req, res);
+    }
     if(req.url.startsWith('/users')){
         return userRt(req, res);
-    }
-    if(req.url.startsWith('/wallets') || req.url.endsWith('/wallets')) {
-        return walletRt(req, res);
     }
 })
 
